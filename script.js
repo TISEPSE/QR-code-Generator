2350061b7d791b2c859cfd7586f54e85ef60452b
TISEPSE <titi21player@gmail.com>
2024-10-18 12:26:45 +0200

    push CSS

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const input = document.getElementById('QrCodeInput')
-const button = document.getElementById('QrCodeButton')
-const img = document.getElementById('QrCodeImage')
+const input = document.getElementById('QrCodeInput');
+        const button = document.getElementById('QrCodeButton');
+        const img = document.getElementById('QrCodeImage');
+        const downloadButton = document.getElementById('QrCodeButtonDownload');
 
-button.addEventListener('click',() =>{
-   QRCode.toDataURL(input.value).then(dataUrl => {
-    img.src = dataUrl
-   })
+        // Génération du QR code
+        button.addEventListener('click', () => {
+            const qrText = input.value.trim();
+            
+            // Vérifie que le champ d'input n'est pas vide
+            if (qrText === "") {
+                alert("Veuillez entrer une URL.");
+                return;
+            }
 
-});
+            QRCode.toDataURL(qrText)
+                .then(dataUrl => {
+                    // Affiche l'image QR code
+                    img.src = dataUrl;
+
+                    // Met à jour l'attribut href pour le bouton de téléchargement
+                    downloadButton.href = dataUrl; // Attribue le dataURL pour le téléchargement
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        });
