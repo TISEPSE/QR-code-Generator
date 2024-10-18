442b584569acd407b160901891a1a237c9c71010
TISEPSE <titi21player@gmail.com>
2024-10-18 10:36:03 +0200

    push

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-import {toDataURL} from "qrcode";
-
 const input = document.getElementById('QrCodeInput')
 const button = document.getElementById('QrCodeButton')
-const img = doucment.getElementById('QrCodeImage')
+const img = document.getElementById('QrCodeImage')
 
-button.addEventListener('click', async () =>{
-    const dataUrl = await QRCode.toDataURL(input.value)
-
-    img.src = dataUrl;
+button.addEventListener('click',() =>{
+   QRCode.toDataURL(input.value).then(dataUrl => {
+    img.src = dataUrl
+   })
 
 });
